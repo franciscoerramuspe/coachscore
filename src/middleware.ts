0fb51c92dce44614238cbef94e328e5dc447117e
Francisco <franciscoerramuspe91@gmail.com>
2024-08-27 20:25:28 +0200

    middleware refactor

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,26 +1,15 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
-import { connect } from './lib/db';
 
 const isProtectedRoute = createRouteMatcher([
   '/pages/search(.*)',
   '/pages/rate(.*)',
 ]);
 
-export default clerkMiddleware(async (auth, req) => {
+export default clerkMiddleware((auth, req) => {
   if (isProtectedRoute(req)) {
-    await auth().protect();
-    try {
-      await connect();
-      console.log(
-        'Conexión a la base de datos verificada después del inicio de sesión'
-      );
-    } catch (error) {
-      console.error(
-        'Error al verificar la conexión a la base de datos:',
-        error
-      );
-    }
+    return auth().protect();
   }
+  return null;
 });
 
 export const config = {
